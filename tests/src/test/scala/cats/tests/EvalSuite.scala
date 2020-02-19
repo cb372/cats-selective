bbbc75743cb125530262dab59e3d61edba36f684
Ian Murray <ian.c.murray@gmail.com>
2020-02-19 18:31:24 +0000

    Test that Eval satisifes the rigid selective laws

---
+++ tests/src/test/scala/cats/tests/EvalSuite.scala
@@ -0,0 +1,17 @@
+package cats.tests
+
+import cats.data._
+import cats.laws.discipline._
+import org.scalacheck.Arbitrary._
+import cats.laws.discipline.arbitrary._
+import cats.Eval
+import cats.Selective
+
+// TODO: abstract this to a general Monad because all monadic selective functors are rigid
+
+class EvalSuite extends CatsSuite {
+
+  implicit val selective = Selective.fromMonad[Eval]
+
+  checkAll("Eval[Int]", RigidSelectiveTests[Eval].selective[Int, Int, Int])
+}
